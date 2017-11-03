f7fef0421dab02fdfb4cd446af9dd4592e699c69
Ailton Martins <pinho123@gmail.com>
2017-11-03 00:42:39 -0200

    Install Firebase

--- api/index.js
+++ api/index.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import bodyParser from  'body-parser';
 import morgan from 'morgan';
-import cors from 'cors'
+import cors from 'cors';
+import firebaseAdmin from 'firebase-admin';
 
 import routes from './routes';
 
 const app = express();
+
+let serviceAccount = require("./bravi-91b8f-firebase-adminsdk-phdo4-4922ff84f7.json");
+
+app.firebase = firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(serviceAccount),
+    databaseURL: "https://bravi-91b8f.firebaseio.com"
+});
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
