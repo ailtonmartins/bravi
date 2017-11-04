f7163a8dd891114e4587750690c347c79308df79
Ailton Martins <pinho123@gmail.com>
2017-11-04 00:50:58 -0200

    Create component and service Clima

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClimaComponent } from './clima/clima.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClimaComponent
   ],
   imports: [
     BrowserModule
